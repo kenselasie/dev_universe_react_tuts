deedec533ef178104c8cfbe966b007a59ede0706
kenselasie <kenselasie@gmail.com>
2022-01-17 09:58:51 +0000

    Update: Todo View

---
+++ src/Views/TodoPage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Todo from '../Components/Todo.js'
+
+function TodoPage() {
+    return (
+        <div>
+            <Todo />
+        </div>
+    )
+}
+
+export default TodoPage
