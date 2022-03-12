d8b3b3927e2d50933c92561b588285149f5fcb08
princenedjoh <princenedjoh5@gmail.com>
2022-03-12 22:17:57 +0000

    1.fixed todo app 2.ability to add and remove todos 3.fixed dashboard positioning
    thank you

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -10,30 +10,41 @@ function Todo() {
         getTodosFromAPI()
     }, [])
 
-    const deleteTodo = (todo) => {
-        console.log(todo)
+    const deleteTodo = (i) => {
+        console.log(i)
         // Pass todo to the delete API here
-
+        let del_todolist_item = todoList
+        del_todolist_item.splice(i,1)
+        setTodoList([...del_todolist_item])
         // Call getTodosFromAPI again to load latest change
-        getTodosFromAPI()
     }
 
     const checkTodoAsCompleted = (todo) => {
         // Got to API and edit the todo
-
-        // Call getTodosFromAPI again to load latest change
-        getTodosFromAPI()
+        if(todo.completed){
+            todo.completed = false
+        }
+        else{todo.completed = true}
+        setTodoList([...todoList])
+        // call setTodoList in order to load changes
     }
 
+
     const addTodoToAPI = () => {
-        console.log(todoInput)
-        let updatedTodo = [...todoList]
-        updatedTodo.push({
-            id: '343493',
-            todoItem: todoInput,
-            completed: false
-        })
-        setTodoList(updatedTodo)
+        if(todoInput.length <= 0){
+            alert('please enter todo')
+        }
+        else{
+            console.log(todoInput)
+            let updatedTodo = [...todoList]
+            updatedTodo.push({
+                id: '343493',
+                todoItem: todoInput,
+                completed: false
+            })
+            setTodoList(updatedTodo)
+            setTodoInput('')
+        }
     }
 
     const getTodosFromAPI = async () => {
@@ -55,6 +66,8 @@ function Todo() {
             console.error(error);
           }
     }
+    let todo_key = 0
+
 
     if (todoList.length > 0) return (
         <div className="wrapper">
@@ -79,9 +92,11 @@ function Todo() {
                                     { todo.completed ? 
                                         <p className="todo__text"><del>{todo.todoItem}</del></p> 
                                         : 
-                                        <p className="todo__text">{todo.todoItem}</p>}   
+                                        <p className="todo__text">{todo.todoItem}</p>
+                                    }   
                                 </div>
-                                <button onClick={() => deleteTodo(todo)} className="delete__btn">X</button>
+                                <button onClick={() => deleteTodo(i)} className="delete__btn">X</button>
+                                
                             </div>
                         </div>
                     ))
--- src/Views/Dashboard.js
+++ src/Views/Dashboard.js
@@ -28,89 +28,93 @@ function Dashboard() {
         <>  
             <div className='wrapper2'>
                 <div className='side__pannel'>
-                    <section className='profile_info'>
-                        <div className='user_image'>
-                            <img className='profile__image' src='./images/user profile.jpg' alt='profile' />
-                        </div>
-                        <div className='user_name' >
-                            Prince Nedjoh
-                        </div>
-                        <div>
-                            <Link to="/todo"><a>My Profile</a></Link> 
-                        </div>
-                        <div>
-                            <hr style={{margin: '20px'}} className='hr_general'/>
-                        </div>
-                    </section>
+                    <div className="menu_items">
+                        <section className='profile_info'>
+                            <div className='user_image'>
+                                <img className='profile__image' src='./images/user profile.jpg' alt='profile' />
+                            </div>
+                            <div className='user_name' >
+                                Prince Nedjoh
+                            </div>
+                            <div>
+                                <Link to="/todo"><a>My Profile</a></Link> 
+                            </div>
+                            <div>
+                                <hr style={{margin: '20px'}} className='hr_general'/>
+                            </div>
+                        </section>
+                        
                     
-                   
-            
-
-                    <div className="menu__text">
-                        <strong>MENU</strong>
-                    </div>
-
-                    <div className='menu__items'>
-                        <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< RiInboxUnarchiveLine /></div><div>Inbox</div></div>
-                        <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< MdFilePresent /></div><div>Sent</div></div>
-                        <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< MdOutlineDrafts /></div><div>Drafts</div></div>
-                        <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< AiFillDelete /></div><div>Deleted</div></div>
-                        <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< MdOutlineFavoriteBorder /></div><div>Favourites</div></div>
-                        <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< SiGoogleanalytics /></div><div>Analytics</div></div>
-                        <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< MdPublishedWithChanges /></div><div>Changes</div></div>
-                        <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< FaMoneyBillAlt /></div><div>Income</div></div>
-                    </div>
-
-                    <div className='bottom__btns'>
-                        <button className='btn btn1'>Upgrade Plan</button>
-                        <button className='btn btn2'>Contact Support</button>
-                    </div>
                 
+                        <div className="decoy_with_chart">
+                                <div className="menu__text">
+                                    <strong>MENU</strong>
+                                </div>
+
+                                <div className='menu__items'>
+                                    <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< RiInboxUnarchiveLine /></div><div>Inbox</div></div>
+                                    <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< MdFilePresent /></div><div>Sent</div></div>
+                                    <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< MdOutlineDrafts /></div><div>Drafts</div></div>
+                                    <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< AiFillDelete /></div><div>Deleted</div></div>
+                                    <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< MdOutlineFavoriteBorder /></div><div>Favourites</div></div>
+                                    <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< SiGoogleanalytics /></div><div>Analytics</div></div>
+                                    <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< MdPublishedWithChanges /></div><div>Changes</div></div>
+                                    <div className = 'menu__items_individual'><div className='menu__items_individual_icon'>< FaMoneyBillAlt /></div><div>Income</div></div>
+                                </div>
+                        </div>
+                    </div>
+
+                            <div className='bottom__btns'>
+                                <button className='btn btn1'>Upgrade Plan</button>
+                                <button className='btn btn2'>Contact Support</button>
+                            </div>
                 </div>
                 <div className='side__pannel_decoy'>
-
-                </div>
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                
-                <div className='main__view'>
-                    <section className='header'>
-                        <div className = 'overview'>Overview</div>
-                        <div className='header_rule'></div>
-                    </section>
-                    <section className='view'>
-                        <div>
-                            <MainView />
-                        </div>
                         
-                    </section>
-                    <section className='table__container'>
-                        <div className='table'>
-                            <Table />
-                        </div>
-                    </section>
-
-
-                <div>
-                    <Users_area />
                 </div>
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                
+                <div className="main__view_h">
+                    
+                    <div className='main__view'>
+                        <section className='header'>
+                            <div className = 'overview'>Overview</div>
+                            <div className='header_rule'></div>
+                        </section>
+                        <section className='view'>
+                            <div>
+                                <MainView />
+                            </div>
+                            
+                        </section>
+                        <section className='table__container'>
+                            <div className='table'>
+                                <Table />
+                            </div>
+                        </section>
+
+
+                        <div>
+                            <Users_area />
+                        </div>
 
 
 
+                    </div>
                 </div>
             </div>
             
